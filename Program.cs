ab9b59a7acbf6b9569475a2d990d9c71e6ef6a6a
Malabeadman <deangelo.lee19@gmail.com>
2023-10-30 23:56:49 -0400

    reverseWords

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+ReverseWords("Jack");
+string ReverseWords(string s)
+{
+    Console.WriteLine(s);
+    char[] reverseChar = s.ToCharArray();
+    char[] backwardsWord = reverseChar;
+    int lengthWord = s.Length - 1;
+    for (int i = 0; i < s.Length; i++)
+    {
+        Console.WriteLine(i);
+        Console.WriteLine(s.Length);
+        Console.WriteLine(reverseChar[(lengthWord) - i]);
+        backwardsWord[i] = reverseChar[(s.Length - 1) - i];
+
+    }
+    Console.WriteLine(backwardsWord);
+    string wordTwoString = new string(backwardsWord);
+
+    Console.WriteLine(wordTwoString);
+    return "outer Done";
+
+}

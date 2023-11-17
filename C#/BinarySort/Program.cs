c27249bcb931f66cc378ddf6984cc214f697a63d
Malabeadman <deangelo.lee19@gmail.com>
2023-11-16 22:32:15 -0500

    Sorted bitwise

--- C#/BinarySort/Program.cs
+++ C#/BinarySort/Program.cs
@@ -6,27 +6,31 @@ Console.WriteLine("Hello, World!");
 int[] xBits = { 22, 24, 11, 12, 9, 2, 4, 92, 89 };
 SortByBits(xBits);
 
-// loop through the array
-/// use Array Sort to sort in asscending order
-static int[] SortByBits(int[] arr)
-{
-    int[] sortBits = new int[arr.Length - 1];
-    int[] binaryBits = new int[arr.Length - 1];
+ public int[] SortByBits(int[] arr) {
+// loop through the array and taking number on the right if its larger          /// than the number on the left take number on right and move it to the front
+    int[] sortBits = new int[arr.Length];
 
-    Array.Sort(arr);
-    int loopcount = 0;
-    foreach (int i in arr)
-    {
-        Console.WriteLine(i);
-        sortBits[loopcount] = i;
-        string binaryBit = Convert.ToString(i, 2);
-        binaryBits[loopcount] = i;
-        Console.WriteLine(binaryBits[loopcount]);
-
-        loopcount++;
+    // custom compair by 1s in the numbers in the array 
+    // var used to call private method for custom compair
+    Array.Sort(arr, (frst, scnd) => {
+        int firstBit = SortBinary(frst);
+        int secondBit = SortBinary(scnd);
+// check to see if both numbers are equal
+        if(firstBit != secondBit){
+            return firstBit - secondBit;
+        }
+        return frst - scnd;
+    });
+    return arr;
     }
-    Console.WriteLine("Sorted Array End");
-    return sortBits;
-}
 
+    private int SortBinary(int x){
+        int placeholder = 0;
 
+        while(x > 0){
+            placeholder += x & 1;
+            // bitwise right shift operater number of bits defined by its         right-hand operand
+            x >>= 1;
+        }
+        return placeholder; 
+    }

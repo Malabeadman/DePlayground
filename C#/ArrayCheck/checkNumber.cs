825db2c08a199ae90ecf74ebd33b25b1be1e52f8
Malabeadman <deangelo.lee19@gmail.com>
2023-11-17 23:58:02 -0500

    Array Check started

---
+++ C#/ArrayCheck/checkNumber.cs
@@ -0,0 +1,17 @@
+﻿
+using System;
+using Internal;
+
+public class Solution
+{
+    public int MaxFrequency(int[] nums, int k)
+    {
+
+    }
+    public void Greeting()
+    {
+        Console.WriteLine("Hellooo, World!");
+
+    }
+    Greeting();
+}
